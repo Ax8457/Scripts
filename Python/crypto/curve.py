22eeea94dee841a3307829d067dea38e34df952c
Ax8457 <147101606+Ax8457@users.noreply.github.com>
2026-02-13 18:44:52 +0100

    Update curve.py

--- Python/crypto/curve.py
+++ Python/crypto/curve.py
@@ -1,47 +1,72 @@
 ##https://cryptohack.org/courses/elliptic/ecc1/
+## https://cryptohack.org/courses/elliptic/ecc2/
+## https://cryptohack.org/courses/elliptic/ecc3/
 ## Point addition
+## Scalar Multiplication
+## Share Secret Computation
 
 class Curve:
+
     def __init__(self, a, p):
-        self.a = a  
-        self.p = p 
+        self.a = a
+        self.p = p
 
     def add(self, P, Q):
-        if P is None: return Q
-        if Q is None: return P
-        
-        x1, y1 = P
-        x2, y2 = Q
+        if P is None:
+            return Q
+        if Q is None:
+            return P
 
-        if x1 == x2 and (y1 + y2) % self.p == 0 : # because of the group in which we are working Fp
+        (x1, y1) = P
+        (x2, y2) = Q
+
+        if x1 == x2 and (y1 + y2) % self.p == 0:  # because of the group in which we are working Fp
             return None
-	
-	# compute lambda
+
         if P != Q:
             num = (y2 - y1) % self.p
             den = (x2 - x1) % self.p
         else:
             num = (3 * pow(x1, 2) + self.a) % self.p
-            den = (2 * y1) % self.p
+            den = 2 * y1 % self.p
 
-        lbd = (num * pow(den, -1, self.p)) % self.p
+        lbd = num * pow(den, -1, self.p) % self.p
         x3 = (pow(lbd, 2) - x1 - x2) % self.p
         y3 = (lbd * (x1 - x3) - y1) % self.p
-        
+
         return (x3, y3)
 
-P=(493,5564)
-Q=(1539,4742)
-R=(4403,5202)
+    def scalar_mul(self, P, n):
+        Q = P
+        R = None
 
-#P + P + Q +R
-C = Curve(a=497, p=9739)
-temp = C.add(P,P)
-temp = C.add(temp,Q)
-res = C.add(temp,R)
-print(res)
+        while n > 0:
+            if n % 2 == 1:
+                R = self.add(R, Q)
 
-X = (5274,2841)
-Y=(8669,740)
-print(C.add(X,Y))
-print(C.add(X,X))
+            Q = self.add(Q, Q)
+            n = n // 2
+        return R
+
+    def compute_SharedSecret(self, PubKey, PrivKey):
+        try:
+            shared_secret = self.scalar_mul(PubKey, PrivKey)
+        except ValueError:
+            print('[X] Error')
+
+        return shared_secret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
