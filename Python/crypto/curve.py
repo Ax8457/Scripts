cb4cc31719470ab990ca5a1ab39b5ba2a7c5566b
Ax8457 <147101606+Ax8457@users.noreply.github.com>
2026-02-14 03:08:18 +0100

    Update curve.py

--- Python/crypto/curve.py
+++ Python/crypto/curve.py
@@ -1,15 +1,18 @@
 ##https://cryptohack.org/courses/elliptic/ecc1/
 ## https://cryptohack.org/courses/elliptic/ecc2/
+## ****
 ## https://cryptohack.org/courses/elliptic/ecc3/
 ## Point addition
 ## Scalar Multiplication
 ## Share Secret Computation
+## Mongomery curve
+from sympy.ntheory import sqrt_mod
 
 class Curve:
-
-    def __init__(self, a, p):
+    def __init__(self, a, p, b):
         self.a = a
         self.p = p
+        self.b = b
 
     def add(self, P, Q):
         if P is None:
@@ -56,8 +59,66 @@ class Curve:
 
         return shared_secret
 
+    def compute_y_from_x(self, x): ## works only if x is the coordinate of a point on the curve
+        y_2 = (x**3 + self.a*x + self.b) % self.p
+        y = sqrt_mod(y_2, self.p) ## tonelli shanks for squarred root moduli
+        return y
 
+class MongomeryCurve:
+    def __init__(self, A, B, p):
+        self.A = A
+        self.B = B
+        self.p = p
 
+    def mong_add(self, P, Q):
+        if P is None: return Q
+        if Q is None: return P
+        if P == Q: return self.mong_double(P)
+        
+        x1, y1 = P
+        x2, y2 = Q
+        num = (y2 - y1) % self.p
+        den = (x2 - x1) % self.p
+        alpha = (num * pow(den, -1, self.p)) % self.p
+        x3 = (self.B * pow(alpha, 2) - self.A - x1 - x2) % self.p
+        y3 = (alpha * (x1 - x3) - y1) % self.p
+        return (x3, y3)
+
+    def mong_double(self, P):
+        if P is None: return None
+        x1, y1 = P
+        num = (3 * pow(x1, 2) + 2 * self.A * x1 + 1) % self.p
+        den = (2 * self.B * y1) % self.p
+        alpha = (num * pow(den, -1, self.p)) % self.p
+        x3 = (self.B * pow(alpha, 2) - self.A - 2 * x1) % self.p
+        y3 = (alpha * (x1 - x3) - y1) % self.p
+        return (x3, y3)
+
+    def mong_scalar_mul(self, P, k):
+        n = k.bit_length()
+        if n == 0: return None
+        
+        # Init
+        R0 = P
+        R1 = self.mong_double(P)
+        
+        i = n - 2
+        while i >= 0:
+            k_i = (k >> i) & 1
+            if k_i == 0:
+                R1 = self.mong_add(R0, R1)
+                R0 = self.mong_double(R0)    
+            else:
+                R0 = self.mong_add(R0, R1)
+                R1 = self.mong_double(R1)
+            i -= 1
+        return R0
+
+    def compute_y_from_x_montgomery(self, x):
+    #B = 1
+        y_2 = (pow(x, 3, self.p) + self.A * pow(x, 2, self.p) + x) % self.p
+        y = sqrt_mod(y_2, self.p)
+        return int(y)
 
 
 
